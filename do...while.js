bb5d41985d82b9c51b540e43dc8268432ea716aa
NathanJSDev <138613934+NathanJSDev@users.noreply.github.com>
2023-11-03 15:39:20 -0300

    Create do...while.js

---
+++ do...while.js
@@ -0,0 +1,19 @@
+var a = 0;
+var t = "";
+do{
+    a += 1;
+    t += " " + a;
+}while(a < 5);
+console.log(t);
+
+// Expected output: " 1 2 3 4 5"
+
+var a = 0;
+var t = "";
+do{
+    a += 1;
+    t += " " + a;
+}while(a < 0);
+console.log(t);
+
+// Expected output: " 1"
