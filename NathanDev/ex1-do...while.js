aa251856a0fc1a239f74f6a813d0c105489fcd87
NathanJSDev <138613934+NathanJSDev@users.noreply.github.com>
2023-11-09 15:10:21 -0300

    Structure Update
    Code examples are now organized by the publisher username.

---
+++ NathanDev/ex1-do...while.js
@@ -0,0 +1,9 @@
+var a = 0;
+var t = "";
+do{
+    a += 1;
+    t += " " + a;
+}while(a < 5);
+console.log(t);
+
+// Expected output: " 1 2 3 4 5"
---
+++ NathanDev/ex2-do...while.js
@@ -0,0 +1,9 @@
+var a = 0;
+var t = "";
+do{
+    a += 1;
+    t += " " + a;
+}while(a < 0);
+console.log(t);
+
+// Expected output: " 1"
